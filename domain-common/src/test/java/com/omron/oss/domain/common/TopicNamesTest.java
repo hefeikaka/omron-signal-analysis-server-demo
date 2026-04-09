2a727020aaf26e69cf82c07997416caa3f8a4575
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:13:50 +0800

    Initial demo backup

---
+++ domain-common/src/test/java/com/omron/oss/domain/common/TopicNamesTest.java
@@ -0,0 +1,14 @@
+package com.omron.oss.domain.common;
+
+import com.omron.oss.domain.common.constants.TopicNames;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TopicNamesTest {
+
+    @Test
+    void shouldKeepCompatibleSourceSignalTopicName() {
+        assertEquals("sourceSiganlCurve", TopicNames.SOURCE_SIGNAL_CURVE);
+    }
+}
