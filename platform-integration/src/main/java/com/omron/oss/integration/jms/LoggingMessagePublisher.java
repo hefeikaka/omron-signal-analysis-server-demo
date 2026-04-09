ff10509e9413e06022fcd6bd41b37fff100a9250
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:14:45 +0800

    Initial demo backup

---
+++ platform-integration/src/main/java/com/omron/oss/integration/jms/LoggingMessagePublisher.java
@@ -0,0 +1,13 @@
+package com.omron.oss.integration.jms;
+
+import java.util.logging.Logger;
+
+public final class LoggingMessagePublisher implements MessagePublisher {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingMessagePublisher.class.getName());
+
+    @Override
+    public void publish(String topicName, String payload) {
+        LOGGER.info("Publishing to topic '" + topicName + "' payload=" + payload);
+    }
+}
