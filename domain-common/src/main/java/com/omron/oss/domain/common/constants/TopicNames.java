51977f53b587a1c6f538bd3533ce7c9d1fb09383
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:13:40 +0800

    Initial demo backup

---
+++ domain-common/src/main/java/com/omron/oss/domain/common/constants/TopicNames.java
@@ -0,0 +1,17 @@
+package com.omron.oss.domain.common.constants;
+
+public final class TopicNames {
+
+    public static final String SOURCE_SIGNAL_CURVE = "sourceSiganlCurve";
+    public static final String MACHINE_CURVE_SAVE = "machine_curve.save";
+    public static final String SIGNAL_MACHINE_INDEX_HISTORY = "signal_machine_index_history_store";
+    public static final String SIGSTA_ETL = "sigsta_etl";
+    public static final String SIGFFT_ETL = "sigfft_etl";
+    public static final String SIGBFF_ETL = "sigbff_etl";
+    public static final String ENVSPECTRUM_ETL = "envspectrum_etl";
+    public static final String BFF_ETL = "bff_etl";
+    public static final String SIGNAL_WS_ETL = "signal_ws_etl";
+
+    private TopicNames() {
+    }
+}
