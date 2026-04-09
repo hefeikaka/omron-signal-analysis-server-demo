e23879135cf1e0734b26dd9719d7b4fe6e3dbb06
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:14:10 +0800

    Initial demo backup

---
+++ domain-signal-storage/src/main/java/com/omron/oss/domain/signal/storage/SignalStorageCollections.java
@@ -0,0 +1,11 @@
+package com.omron.oss.domain.signal.storage;
+
+public final class SignalStorageCollections {
+
+    public static final String RAWDATA_SIGNAL_CURVE = "rawdata_signal_curve";
+    public static final String RAWDATA_SIGNAL_ANOMALY_SOURCE = "rawdata_signal_anormaly_source";
+    public static final String RAWDATA_INDEX_SOURCE = "rawdata_index_source";
+
+    private SignalStorageCollections() {
+    }
+}
