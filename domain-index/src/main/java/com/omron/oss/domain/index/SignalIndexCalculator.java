feaff18ea64db6d7a7f223fc229fdb351bbf8142
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:13:52 +0800

    Initial demo backup

---
+++ domain-index/src/main/java/com/omron/oss/domain/index/SignalIndexCalculator.java
@@ -0,0 +1,14 @@
+package com.omron.oss.domain.index;
+
+import com.omron.oss.domain.common.model.SignalSample;
+
+public final class SignalIndexCalculator {
+
+    public double rms(SignalSample sample) {
+        double sumSquares = 0.0d;
+        for (Double value : sample.getValues()) {
+            sumSquares += value * value;
+        }
+        return Math.sqrt(sumSquares / sample.getValues().size());
+    }
+}
