e4dfb0e22031deb16eaf67802b835b0bc697f866
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:13:42 +0800

    Initial demo backup

---
+++ domain-common/src/main/java/com/omron/oss/domain/common/model/NormalizedSignalMessage.java
@@ -0,0 +1,26 @@
+package com.omron.oss.domain.common.model;
+
+public final class NormalizedSignalMessage {
+
+    private final String topicName;
+    private final SignalSample sample;
+    private final String payloadJson;
+
+    public NormalizedSignalMessage(String topicName, SignalSample sample, String payloadJson) {
+        this.topicName = topicName;
+        this.sample = sample;
+        this.payloadJson = payloadJson;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public SignalSample getSample() {
+        return sample;
+    }
+
+    public String getPayloadJson() {
+        return payloadJson;
+    }
+}
