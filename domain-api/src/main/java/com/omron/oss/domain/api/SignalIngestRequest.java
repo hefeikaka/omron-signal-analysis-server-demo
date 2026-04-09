42dfa4d8c9719f57fd9d01dd30393e47ccb655db
hefeikaka <handongkaka@foxmail.com>
2026-04-10 00:13:33 +0800

    Initial demo backup

---
+++ domain-api/src/main/java/com/omron/oss/domain/api/SignalIngestRequest.java
@@ -0,0 +1,34 @@
+package com.omron.oss.domain.api;
+
+import java.util.List;
+
+public final class SignalIngestRequest {
+
+    private String machineId;
+    private int samplingFrequency;
+    private List<Double> values;
+
+    public String getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(String machineId) {
+        this.machineId = machineId;
+    }
+
+    public int getSamplingFrequency() {
+        return samplingFrequency;
+    }
+
+    public void setSamplingFrequency(int samplingFrequency) {
+        this.samplingFrequency = samplingFrequency;
+    }
+
+    public List<Double> getValues() {
+        return values;
+    }
+
+    public void setValues(List<Double> values) {
+        this.values = values;
+    }
+}
